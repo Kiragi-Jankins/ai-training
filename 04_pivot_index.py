cd585749681698d3c0c0da437102a647a2ee7e28
Kiragi_Jankins <Kiragi.Jankins@yandex.ru>
2026-06-15 08:54:31 +0900

    Удалил дубликаты

--- 04_pivot_index.py
+++
@@ -1,9 +0,0 @@
-def pivot_index(nums):
-    summa = sum(nums)
-    tmp_sum = 0
-    for i in range(len(nums)):
-        if tmp_sum == summa - tmp_sum - nums[i]:
-            return i
-        tmp_sum += nums[i]
-    else:
-        return -1

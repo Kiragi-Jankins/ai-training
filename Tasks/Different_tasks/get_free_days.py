229d1b34918374a5edd2dd924d3a83418fe8830c
Kiragi_Jankins <Kiragi.Jankins@yandex.ru>
2026-06-23 15:19:01 +0900

    refactor: separete buisness logic and I/O operations

--- Tasks/Different_tasks/get_free_days.py
+++ Tasks/Different_tasks/get_free_days.py
@@ -1,9 +1,14 @@
 import calendar
 import datetime
 
-def get_free_days(year: int) -> None:
+def get_free_days(year: int) -> list[datetime.date]:
+    result = []
     for month_idx in range(1, 13):
         for week in calendar.monthcalendar(year, month_idx):
             free_day = week[3]
             if 15 <= free_day <= 21:
-                print(datetime.date(year, month_idx, free_day).strftime('%d.%m.%Y'))
+                result.append(datetime.date(year, month_idx, free_day))
+    return result
+
+for data in get_free_days(int(input())):
+    print(data.strftime('%d.%m.%Y'))
